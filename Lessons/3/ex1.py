8649c7ae0dcce27aa04daec6f278898d940bc772
Bartłomiej Kręgielewski <bartlomiejkregielewski@gmail.com>
2025-09-09 23:37:50 +0200

    Lesson 3 - Prepared

---
+++ Lessons/3/ex1.py
@@ -0,0 +1,6 @@
+"""
+Stwórz klasy: `Kolo` i `Kwadrat`.
+Klasy powinny przechowywać informację o figurze (promień lub bok).
+Dodaj metody do obliczania pola i obwodu.
+Pamiętaj, aby stworzyć odpowiedni konstruktor.
+"""
---
+++ Lessons/3/main.py
@@ -0,0 +1,10 @@
+class Samochod:
+    marka = ""
+    model = ""
+    typSilnika = ""
+    mocKM = 0
+
+    def wyswietl(self):
+        print(self.marka)
+        print(self.model)
+        print(self.typSilnika, self.mocKM)
