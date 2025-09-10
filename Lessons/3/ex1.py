a572a8496a539449a597908e87f771af51827812
Bartłomiej Kręgielewski <bartlomiejkregielewski@gmail.com>
2025-09-10 20:00:53 +0200

    Lesson 3 - Completed

--- Lessons/3/ex1.py
+++ Lessons/3/ex1.py
@@ -4,3 +4,50 @@ Klasy powinny przechowywać informację o figurze (promień lub bok).
 Dodaj metody do obliczania pola i obwodu.
 Pamiętaj, aby stworzyć odpowiedni konstruktor.
 """
+
+from math import pi, pow
+
+class Kolo:
+    def __init__(self, promien):
+        self.promien = promien
+
+    def pole(self):
+        return pi * pow(self.promien, 2)
+    
+    def obwod(self):
+        obwod = 2 * pi * self.promien
+        return obwod
+    
+    def wyswietl(self):
+        print(f"To jest koło o promieniu {self.promien}, który ma pole {self.pole()} i obwód {self.obwod()}")
+
+class Kwadrat:
+    def __init__(self, bok):
+        self.bok = bok
+        # self.obwod = 4 * bok
+    
+    def obwod(self):
+        return 4 * self.bok
+    
+    def pole(self):
+        return pow(self.bok, 2)
+    
+    def wyswietl(self):
+        print(f"To jest kwadrat o bok {self.bok}, który ma pole {self.pole()} i obwód {self.obwod()}")
+
+
+# Testy
+kolo1 = Kolo(10)
+# print(kolo1.obwod())
+# print(kolo1.pole())
+kolo1.wyswietl()
+
+kwadrat1 = Kwadrat(2)
+kwadrat1.bok = 6
+
+# print(kwadrat1.obwod)
+
+# print(kwadrat1.obwod())
+# print(kwadrat1.pole())
+
+kwadrat1.wyswietl()
--- Lessons/3/main.py
+++ Lessons/3/main.py
@@ -3,8 +3,26 @@ class Samochod:
     model = ""
     typSilnika = ""
     mocKM = 0
+    LicznikSamochodow = 0
 
     def wyswietl(self):
         print(self.marka)
         print(self.model)
         print(self.typSilnika, self.mocKM)
+
+    def __init__(self, marka, model, typSilnika, mocKM):
+        print("Utworzenie obiektu klasy Samochod")
+        self.marka = marka
+        self.model = model
+        self.typSilnika = typSilnika
+        self.mocKM = mocKM
+        Samochod.LicznikSamochodow += 1
+       
+
+print(Samochod.LicznikSamochodow)
+auto1 = Samochod("Ford", "Focus", "Benzyna", 180)
+print(Samochod.LicznikSamochodow)
+auto2 = Samochod("BMW", "X3", "Benzyna", 180)
+print(Samochod.LicznikSamochodow)
+
+auto1.wyswietl()
