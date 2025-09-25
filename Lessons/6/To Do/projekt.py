08e18219ff7754c72f1072ed105687904d6d5f49
Bartłomiej Kręgielewski <bartlomiejkregielewski@gmail.com>
2025-09-25 18:26:42 +0200

    Lesson 6 - Prepared

---
+++ Lessons/6/To Do/projekt.py
@@ -0,0 +1,12 @@
+# dodanie modułu pygame
+import pygame
+
+# dodanie modułu OS
+import os
+
+# Zmiana obecnej ścieżki pracy
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+# Utworzenie stałych
+SZEROKOSC_EKRANU = 800
+WYSOKOSC_EKRANU = 600
