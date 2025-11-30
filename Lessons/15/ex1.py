d81350ae2ae9f3046c91b30831819f22f4e04db5
Bartłomiej Kręgielewski <bartlomiejkregielewski@gmail.com>
2025-11-30 18:27:53 +0100

    Lesson 15 - Prepared

---
+++ Lessons/15/ex1.py
@@ -0,0 +1,8 @@
+"""
+Napisz program, który będzie działał jak książka telefoniczna.
+Program poinien mieć następujące funkcje:
+- Dodawanie nowego kontaktu - program powinien pytać użytkownika o imię i nazwisko oraz numer telefonu i dodać te dane do listy kontaktów.
+Lista kontaktów powinna być przechowywana w postaci listy słowników, gdzie każdy słownik reprezentuje jeden kontakt.
+- Sortowanie kontaktów za pomocą metody sortowania bąbelkowego - program powinien sortować listę kontaktów alfabetycznie według nazwisk z wykorzystaniem funkcji `bubble_sort`.
+- Wyświetlanie listy kontaktów - program powinien wyświetlić listę kontaktów w formacie: "imię nazwisko - numer telefonu". Kontakty powinny być posortowane alfabetycznie według nazwisk.
+"""
---
+++ Lessons/15/ex2.py
@@ -0,0 +1,10 @@
+"""
+Napisz program, który losuje liczbę z zakresu od 1 do 100.
+Następnie program wyświetli tę liczbę i zacznie zgadywać.
+Użytkownik powinien podać wskazówki dla programu, czy wylosowana liczba jest większa, mniejsza czy równa liczbie zgadywanej przez program.
+
+Podpowiedzi dla programu to:
+- "za mało"
+- "za dużo"
+- "tak"
+"""
