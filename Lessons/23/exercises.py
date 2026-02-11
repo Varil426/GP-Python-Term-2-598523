badd06040739c16b33d4f474ebd3f435d1cdcf19
Bartłomiej Kręgielewski <bartlomiejkregielewski@gmail.com>
2026-02-11 16:19:01 +0100

    Lesson 23 - Prepared

---
+++ Lessons/23/exercises.py
@@ -0,0 +1,171 @@
+import numpy as np
+from numpy import random
+
+
+def print_ex(exercise_number: int):
+    separator_length = 80
+    print("-" * separator_length)
+    print(f"\n\nZadanie {exercise_number}\n\n")
+    print("-" * separator_length)
+
+
+"""
+Zadanie 1
+
+Stwórz funkcję `print_array()` w której zostanie stworzona dwuwymiarowa tablica - `ndarray` oraz wyświetlone zostaną:
+a) Tablica,
+b) Pierwszy element tablicy,
+c) Pierwszy zagnieżdżony element tablicy,
+d) Typ utworzonego obiektu,
+e) Kształt utworzonego obiektu.
+
+Funkcja powinna zwrócić utworzoną tablicę.
+
+"""
+
+print_ex(1)
+
+# Miejsce na kod - Zadanie 1
+
+# Koniec miejsca na kod - Zadanie 1
+
+
+"""
+Zadanie 2
+
+Stwórz funkcję `shapeshifter(arr)` która przyjmuje jako argument utworzoną wcześniej tablicę. Funkcja powinna:
+a)Zmienić rozmiar tablicy na 9x1,
+b)Zmienić rozmiar tablicy na 1x9,
+c)Zmienić rozmiar tablicy na 3x3,
+d)Zmienić rozmiar tablicy na -1x9,
+e)Zmienić rozmiar tablicy na 3x-1,
+f)Podzielić tablicę na 3 nowe tablice.
+
+Wyświetl tablicę na każdym etapie.
+
+"""
+
+print_ex(2)
+
+# Miejsce na kod - Zadanie 2
+
+# Koniec miejsca na kod - Zadanie 2
+
+"""
+Zadanie 3
+
+Utwórz funkcję `data_format`, która zawierać będzie tablicę zawierającą dane
+różnych typów - wcześniej wspomniane zostało że tablice mogą przechowywać dane
+tylko 1 typu, dlatego należy obsłużyć ewentualny wyjątek.
+Dodatkowo utwórz tablice w których dane rzutowane będą kolejno na `string`i, `int`y i `float`y.
+
+"""
+
+print_ex(3)
+
+# Miejsce na kod - Zadanie 3
+
+# Koniec miejsca na kod - Zadanie 3
+
+"""
+Zadanie 4
+
+Utwórz funkcję `sorted_ndarray` w której utworzona zostanie tablica, a następnie
+wyświetlona, oraz posortowana i ponownie wyświetlona.
+
+"""
+
+print_ex(4)
+
+# Miejsce na kod - Zadanie 4
+
+# Koniec miejsca na kod - Zadanie 4
+
+"""
+Zadanie 5
+
+Stwórz funkcję `generate_random_numbers` która wygeneruje 10 losowych liczb
+całkowitych od 0 do 100, oraz 10 losowych liczb typu float z zakresu od 0 do 1.
+
+"""
+
+print_ex(5)
+
+# Miejsce na kod - Zadanie 5
+
+# Koniec miejsca na kod - Zadanie 5
+
+"""
+Zadanie 6
+
+Stwórz funkcję `pick_random_numbers` która:
+a) wygeneruje tablicę losowych liczb z zakresu od 0 do 1 o wymiarach 3x5,
+b) wybierze losową liczbę ze zbioru liczb 3, 5, 7, 9,
+c) utworzy tablicę o wymiarach 3x5 z losowych elementów ze zbioru liczb 3, 5, 7, 9,
+d) wygeneruje 100 losowych liczb, korzystając ze zbioru liczb 3, 5, 7, 9, gdzie szansa na 3 wynosi 10%, na 5-30 %, na 7-60%, a na 9-0%.
+
+"""
+
+print_ex(6)
+
+# Miejsce na kod - Zadanie 6
+
+# Koniec miejsca na kod - Zadanie 6
+
+"""
+Zadanie 7
+
+Stwórz funkcję `shuffle_ndarray` w której przemieszasz tablicę oraz utworzysz permutację tablicy.
+Permutacja - zmiana kolejności elementów w tablicy.
+
+"""
+
+print_ex(7)
+
+# Miejsce na kod - Zadanie 7
+
+# Koniec miejsca na kod - Zadanie 7
+
+"""
+Zadanie 8
+
+Stwórz funkcję `array_functions` w której:
+a) wygenerujesz tablicę z 11 wartościami równo rozmieszczonymi na przedziale od 0 do 10,
+b) wyświetlisz tą tablicę,
+c) wyświetlisz sumę elementów tej tablicy,
+d) wyświetlisz najmniejszy element tablicy,
+e) wyświetlisz największy element tablicy,
+f) wyświetlisz średnią wartość tablicy,
+g) wyświetlisz wariancje elementów tablicy,
+h) wyświetlisz odchylenie standardowe elementów tablicy.
+
+Wariacja - miara rozproszenia danych wokół średniej arytmetycznej.
+Odchylenie standardowe - miara rozproszenia wyników średniej arytmetycznej, ukazująca jak bardzo wartości w zestawie danych różnią się od średniej arytmetycznej tych danych.
+
+"""
+
+print_ex(8)
+
+# Miejsce na kod - Zadanie 8
+
+# Koniec miejsca na kod - Zadanie 8
+
+"""
+Zadanie 9
+
+Utwórz funkcję `mathematical_functions` w której:
+a) wyświetlisz wartość liczby pi,
+b) wyświetlisz wartość liczby eulera,
+c) wyświetlisz wartość kwadratu liczby eulera,
+d) wyświetlisz wartość sinus pi/2,
+e) wyświetlisz wartość cosinus pi/2,
+f) wyświetlisz wartość tangens pi/2,
+g) wyświetlisz wartość pi/2 zamienioną na stopnie i 360 stopni zamienioną na radiany.
+
+"""
+
+print_ex(9)
+
+# Miejsce na kod - Zadanie 9
+
+# Koniec miejsca na kod - Zadanie 9
